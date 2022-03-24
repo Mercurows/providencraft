5acf8003a6df5c097dc0f06fae2bc142e82b4fdd
Light_Quanta <ZCH22248@foxmail.com>
2022-03-24 15:03:52 +0800

    修改了铁坨子的材料类型

--- src/main/java/tech/lq0/providencraft/item/providenceOI/shirako/Fetuozi.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/shirako/Fetuozi.java
@@ -14,14 +14,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
 public class Fetuozi extends SwordItem {
     public Fetuozi() {
-        super(ItemTier.IRON, 15, -3.0f, new Item.Properties().maxDamage(1145).group(ModGroup.itemgroup));
+        super(ItemTier.NETHERITE, 15, -3.0f, new Item.Properties().maxDamage(1145).group(ModGroup.itemgroup));
     }
 
     @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         tooltip.add((new TranslationTextComponent("fetuozi_des")).mergeStyle(TextFormatting.GRAY));
     }
