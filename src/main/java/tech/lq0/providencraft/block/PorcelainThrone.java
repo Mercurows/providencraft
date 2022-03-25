3452e4c7a796b9ef14083a0eb29831544edd50fc
17146 <1714673995@qq.com>
2022-03-25 14:13:22 +0800

    第30次提交 完成了白瓷王座

--- src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
+++ src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
@@ -1,10 +1,36 @@
 package tech.lq0.providencraft.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.loot.conditions.BlockStateProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+
+import javax.annotation.Nullable;
 
 public class PorcelainThrone extends Block {
+    private static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
     public PorcelainThrone(){
-        super(Properties.create(Material.CLAY).hardnessAndResistance(2));
+        super(Properties.create(Material.CLAY).hardnessAndResistance(2).notSolid());
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH));
     }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
 }
