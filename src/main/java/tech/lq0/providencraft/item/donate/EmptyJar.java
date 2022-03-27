9f9db50c4574fdea0776c97951496c9961180029
17146 <1714673995@qq.com>
2022-03-27 13:17:42 +0800

    第38次提交 修改了空罐子的描述并添加了属性

--- src/main/java/tech/lq0/providencraft/item/donate/EmptyJar.java
+++ src/main/java/tech/lq0/providencraft/item/donate/EmptyJar.java
@@ -1,7 +1,12 @@
 package tech.lq0.providencraft.item.donate;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
@@ -15,10 +20,12 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.UUID;
 
 public class EmptyJar extends Item {
     public EmptyJar(){
@@ -43,5 +50,17 @@ public class EmptyJar extends Item {
         if(!world.isRemote){
             player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE,40,0));
         }
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(slot,stack);
+        UUID uuid = new UUID(ItemRegistry.EMPTY_JAR.hashCode() + slot.toString().hashCode(), 0);
+        if (slot == EquipmentSlotType.HEAD) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.ARMOR,
+                    new AttributeModifier(uuid, "empty jar modifier", 1.0D, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
     }
 }
