b7a05721c019aff1f2145e1b15a410921f3edb96
17146 <1714673995@qq.com>
2022-04-01 17:42:23 +0800

    第51次提交 做完了红鼻子

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/keroro/RedNose.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/keroro/RedNose.java
@@ -1,21 +1,60 @@
 package tech.lq0.providencraft.item.providencemagicros.keroro;
 
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.SoundCategory;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.init.SoundRegistry;
+import tech.lq0.providencraft.models.RedNoseModel;
 
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class RedNose extends ArmorItem {
     public RedNose(){
         super(ArmorMaterial.LEATHER, EquipmentSlotType.HEAD, new Properties().defaultMaxDamage(302).setNoRepair().group(ModGroup.itemgroup));
     }
 
+    @Nullable
     @Override
-    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-        //这里写打喷嚏的功能
-        super.onArmorTick(stack, world, player);
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new RedNoseModel<>();
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return Utils.MOD_ID + ":textures/models/armor/red_nose_model_texture.png";
+    }
+
+    @SubscribeEvent
+    public static void Event(LivingEvent.LivingJumpEvent event){
+        LivingEntity livingEntity = event.getEntityLiving();
+        if(livingEntity instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) livingEntity;
+            if(!player.world.isRemote){
+                ItemStack itemStack = player.getItemStackFromSlot(EquipmentSlotType.HEAD);
+                if(itemStack.getItem().equals(ItemRegistry.RED_NOSE.get())){
+                    player.addPotionEffect(new EffectInstance(Effects.SPEED, 40, 1));
+                    itemStack.damageItem(1,player, (playerEntity) -> playerEntity.sendBreakAnimation(EquipmentSlotType.HEAD));
+
+                }
+            }
+            player.world.playSound(player, player.getPosition(),
+                    SoundRegistry.KERORO_SNEEZE.get(), SoundCategory.AMBIENT, 0.5f, 1f);
+        }
     }
 }
---
+++ src/main/java/tech/lq0/providencraft/models/RedNoseModel.java
@@ -0,0 +1,43 @@
+package tech.lq0.providencraft.models;// Made with Blockbench 4.2.1
+// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
+// Paste this class into your mod and generate all required imports
+
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public class RedNoseModel<T extends LivingEntity> extends BipedModel<T> {
+	private final ModelRenderer bone;
+	private final ModelRenderer main;
+
+	public RedNoseModel() {
+		super(1.0f);
+		textureWidth = 16;
+		textureHeight = 16;
+
+		main = new ModelRenderer(this);
+		main.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+		bone = new ModelRenderer(this);
+		bone.setRotationPoint(0.0F, -2.0F, -5.0F);
+		main.addChild(bone);
+		bone.setTextureOffset(0, 0).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
+	}
+
+	@Override
+	@ParametersAreNonnullByDefault
+	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		main.copyModelAngles(this.bipedHead);
+		main.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+}
