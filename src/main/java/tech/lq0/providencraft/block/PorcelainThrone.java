f2d70fbcf2dfd36ea88580c6ad0d53f9b22567d1
17146 <1714673995@qq.com>
2022-04-05 23:14:26 +0800

    第62次提交 修改了方块模型

--- src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
+++ src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
@@ -8,6 +8,11 @@ import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
 
@@ -30,4 +35,8 @@ public class PorcelainThrone extends Block {
         return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
     }
 
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return VoxelShapes.or(Block.makeCuboidShape(3,0,3,13,7,13));
+    }
 }
