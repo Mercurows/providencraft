4f19ffb52f27b746c3a83f6bf0c00a1d23025297
17146 <1714673995@qq.com>
2022-04-06 22:27:58 +0800

    第65次提交 添加了部分描述

--- src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
+++ src/main/java/tech/lq0/providencraft/block/PorcelainThrone.java
@@ -4,7 +4,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
@@ -12,9 +14,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class PorcelainThrone extends Block {
     private static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
@@ -39,4 +45,11 @@ public class PorcelainThrone extends Block {
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return VoxelShapes.or(Block.makeCuboidShape(3,0,3,13,7,13));
     }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("porcelain_throne_des")).mergeStyle(TextFormatting.GRAY));
+    }
+
+
 }
--- src/main/java/tech/lq0/providencraft/block/RedAhogeBlock.java
+++ src/main/java/tech/lq0/providencraft/block/RedAhogeBlock.java
@@ -2,9 +2,23 @@ package tech.lq0.providencraft.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RedAhogeBlock extends Block {
     public RedAhogeBlock() {
         super(Properties.create(Material.IRON).hardnessAndResistance(3));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("red_ahoge_block_des")).mergeStyle(TextFormatting.GRAY));
     }
 }
