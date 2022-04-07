ffe764b3f8bb881fd630ea176700d265ecd81ff8
17146 <1714673995@qq.com>
2022-04-07 12:11:21 +0800

    第66次提交 添加了电子龙角的模型

--- src/main/java/tech/lq0/providencraft/item/providencesecond/lecia/ElectricHorn.java
+++ src/main/java/tech/lq0/providencraft/item/providencesecond/lecia/ElectricHorn.java
@@ -1,12 +1,32 @@
 package tech.lq0.providencraft.item.providencesecond.lecia;
 
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.models.ElectricHornModel;
+
+import javax.annotation.Nullable;
 
 public class ElectricHorn extends ArmorItem {
     public ElectricHorn() {
         super(ArmorMaterial.DIAMOND, EquipmentSlotType.HEAD, new Properties().defaultMaxDamage(1145).group(ModGroup.itemgroup));
+    }
+
+    @Nullable
+    @Override
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new ElectricHornModel<>();
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return Utils.MOD_ID + ":textures/models/armor/electric_horn_model_texture.png";
     }
 }
---
+++ src/main/java/tech/lq0/providencraft/models/ElectricHornModel.java
@@ -0,0 +1,69 @@
+package tech.lq0.providencraft.models;// Made with Blockbench 4.2.2
+// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
+// Paste this class into your mod and generate all required imports
+
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public class ElectricHornModel<T extends LivingEntity> extends BipedModel<T> {
+	private final ModelRenderer main;
+	private final ModelRenderer bone;
+	private final ModelRenderer bone2;
+
+	public ElectricHornModel() {
+		super(1.0f);
+		textureWidth = 32;
+		textureHeight = 32;
+
+		main = new ModelRenderer(this);
+		main.setRotationPoint(0.0F, 24.0F, 0.0F);
+		
+
+		bone = new ModelRenderer(this);
+		bone.setRotationPoint(0.0F, -3.0F, 0.0F);
+		main.addChild(bone);
+		bone.setTextureOffset(6, 0).addBox(4.0F, -15.0F, -1.0F, 1.0F, 15.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(0, 22).addBox(5.0F, -2.0F, -1.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(18, 3).addBox(5.0F, -3.0F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(10, 16).addBox(5.0F, -4.0F, -1.0F, 3.0F, 1.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(12, 8).addBox(5.0F, -10.0F, -1.0F, 1.0F, 6.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(12, 19).addBox(6.0F, -6.0F, -1.0F, 1.0F, 2.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(18, 17).addBox(4.5F, -14.5F, -1.0F, 1.0F, 2.0F, 2.0F, 0.0F, false);
+		bone.setTextureOffset(6, 17).addBox(3.0F, -7.5F, -1.0F, 1.0F, 3.0F, 2.0F, 0.0F, false);
+
+		bone2 = new ModelRenderer(this);
+		bone2.setRotationPoint(0.0F, -3.0F, 0.0F);
+		main.addChild(bone2);
+		bone2.setTextureOffset(0, 0).addBox(-5.0F, -15.0F, -1.0F, 1.0F, 15.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(16, 21).addBox(-6.0F, -2.0F, -1.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(18, 0).addBox(-7.0F, -3.0F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(16, 6).addBox(-8.0F, -4.0F, -1.0F, 3.0F, 1.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(12, 0).addBox(-6.0F, -10.0F, -1.0F, 1.0F, 6.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(18, 13).addBox(-7.0F, -6.0F, -1.0F, 1.0F, 2.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(18, 9).addBox(-5.5F, -14.5F, -1.0F, 1.0F, 2.0F, 2.0F, 0.0F, false);
+		bone2.setTextureOffset(0, 17).addBox(-4.0F, -7.5F, -1.0F, 1.0F, 3.0F, 2.0F, 0.0F, false);
+	}
+
+	@Override
+	@ParametersAreNonnullByDefault
+	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		this.main.copyModelAngles(this.bipedHead);
+		main.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
