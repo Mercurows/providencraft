322d29b26044752f597ab604d0fbc8238f1cd850
17146 <1714673995@qq.com>
2022-04-17 19:01:51 +0800

    第95次提交 添加了修罗亭的模型材质和功能 修改了版本号

--- src/main/java/tech/lq0/providencraft/item/providencethird/beni/ShuraChin.java
+++ src/main/java/tech/lq0/providencraft/item/providencethird/beni/ShuraChin.java
@@ -1,13 +1,127 @@
 package tech.lq0.providencraft.item.providencethird.beni;
 
-import net.minecraft.item.ItemTier;
-import net.minecraft.item.Rarity;
-import net.minecraft.item.SwordItem;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.monster.GuardianEntity;
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.passive.SquidEntity;
+import net.minecraft.entity.passive.fish.CodEntity;
+import net.minecraft.entity.passive.fish.PufferfishEntity;
+import net.minecraft.entity.passive.fish.SalmonEntity;
+import net.minecraft.entity.passive.fish.TropicalFishEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.tools.ItemNBTTool;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.UUID;
 
 public class ShuraChin extends SwordItem {
+    public static final String TAG_INVOKE = "invoke";
+
     public ShuraChin(){
-        super(ItemTier.IRON, 7, -1.5f, new Properties().rarity(Rarity.UNCOMMON).group(ModGroup.itemgroup).
+        super(ItemTier.IRON, -2, -1.5f, new Properties().rarity(Rarity.UNCOMMON).group(ModGroup.itemgroup).
                 setNoRepair().maxDamage(1442));
+        ItemModelsProperties.registerProperty(this, new ResourceLocation(Utils.MOD_ID, "shurachin_invoke"),
+                (stack, world, entity) -> ItemNBTTool.getBoolean(stack, TAG_INVOKE, false) ? 1.0F : 0.0F);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    @Nonnull
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        if (!worldIn.isRemote && handIn == Hand.MAIN_HAND) {
+            boolean flag = ItemNBTTool.getBoolean(stack, TAG_INVOKE, false);
+            ItemNBTTool.setBoolean(stack, TAG_INVOKE, !flag);
+        }
+        return ActionResult.resultFail(stack);
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(equipmentSlot);
+        UUID uuid = new UUID(ItemRegistry.SHURA_CHIN.hashCode() + equipmentSlot.toString().hashCode(), 0);
+        if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+            map = HashMultimap.create(map);
+            boolean flag = ItemNBTTool.getBoolean(stack, TAG_INVOKE, false);
+            map.put(Attributes.ATTACK_DAMAGE,
+                    new AttributeModifier(uuid, "shurachin modifier", flag ? 12.0f : 0.0f, AttributeModifier.Operation.ADDITION));
+            map.put(Attributes.ATTACK_KNOCKBACK,
+                    new AttributeModifier(uuid, "shurachin modifier", flag ? 0.3f : 0.0f, AttributeModifier.Operation.MULTIPLY_BASE));
+            map.put(Attributes.ARMOR,
+                    new AttributeModifier(uuid, "shurachin modifier", flag ? 0.0f : 8.0f, AttributeModifier.Operation.ADDITION));
+            map.put(Attributes.MOVEMENT_SPEED,
+                    new AttributeModifier(uuid, "shurachin modifier", flag ? 0.0f : 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+        }
+        return map;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(!worldIn.isRemote && entityIn instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) entityIn;
+            if(player.getHeldItemOffhand().getItem().equals(ItemRegistry.UME.get()) &&
+                    player.getHeldItemMainhand().getItem().equals(ItemRegistry.SHURA_CHIN.get())){
+                player.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, 300, 1));
+                player.addPotionEffect(new EffectInstance(Effects.STRENGTH, 300, 1));
+            }
+        }
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if(target instanceof SalmonEntity || target instanceof SquidEntity || target instanceof GuardianEntity ||
+                target instanceof CodEntity || target instanceof PufferfishEntity || target instanceof TropicalFishEntity ||
+        target instanceof ChickenEntity){
+            attacker.addPotionEffect(new EffectInstance(Effects.SATURATION, 100, 1));
+        }
+        return super.hitEntity(stack, target, attacker);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("ume_func")).mergeStyle(TextFormatting.AQUA));
+        tooltip.add((new TranslationTextComponent("shurachin_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("shurachin_des2")).mergeStyle(TextFormatting.GRAY).mergeStyle(TextFormatting.STRIKETHROUGH));
+    }
+
+    @Override
+    public boolean onEntitySwing(ItemStack stack, LivingEntity entity) {
+        return !ItemNBTTool.getBoolean(stack, TAG_INVOKE, false);
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return slotChanged && !oldStack.equals(newStack);
+    }
+
+    @Override
+    public boolean isDamageable() {
+        return false;
     }
 }
