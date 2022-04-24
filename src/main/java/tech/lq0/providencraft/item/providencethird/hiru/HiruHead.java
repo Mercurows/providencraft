1eafa1cf02c622efa06ebebff60847f45a10bffb
17146 <1714673995@qq.com>
2022-04-24 14:51:31 +0800

    第100次提交 添加了hiru头的材质和功能 添加了部分合成表 添加了部分进度

--- src/main/java/tech/lq0/providencraft/item/providencethird/hiru/HiruHead.java
+++ src/main/java/tech/lq0/providencraft/item/providencethird/hiru/HiruHead.java
@@ -1,17 +1,37 @@
 package tech.lq0.providencraft.item.providencethird.hiru;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
 import tech.lq0.providencraft.models.HiruHeadModel;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.UUID;
 
 public class HiruHead extends ArmorItem {
     public HiruHead(){
@@ -28,5 +48,36 @@ public class HiruHead extends ArmorItem {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
         return Utils.MOD_ID + ":textures/models/armor/hiru_head_model_texture.png";
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    @Nonnull
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(equipmentSlot);
+        UUID uuid = new UUID(ItemRegistry.HIRU_HEAD.hashCode() + equipmentSlot.toString().hashCode(), 0);
+        if (equipmentSlot == getEquipmentSlot()) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.MOVEMENT_SPEED,
+                    new AttributeModifier(uuid, "hiru head modifier", -0.1f, AttributeModifier.Operation.MULTIPLY_BASE));
+            map.put(Attributes.KNOCKBACK_RESISTANCE,
+                    new AttributeModifier(uuid, "hiru head modifier", 1.0f, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+        if(!world.isRemote && player.isInWater()){
+            player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 300, 0));
+            player.addPotionEffect(new EffectInstance(Effects.CONDUIT_POWER, 300, 0));
+        }
+        super.onArmorTick(stack, world, player);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("hiru_head_des")).mergeStyle(TextFormatting.GRAY));
     }
 }
