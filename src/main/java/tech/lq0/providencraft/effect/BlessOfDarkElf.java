96ffdba305dbc327d008e903dbb481a7efb2f168
17146 <1714673995@qq.com>
2022-04-29 18:28:41 +0800

    第116次提交 重构了效果类

--- src/main/java/tech/lq0/providencraft/effect/BlessOfDarkElf.java
+++ src/main/java/tech/lq0/providencraft/effect/BlessOfDarkElf.java
@@ -1,10 +1,47 @@
 package tech.lq0.providencraft.effect;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.providencraft.init.EffectRegistry;
 
+import java.util.Objects;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BlessOfDarkElf extends Effect {
     public BlessOfDarkElf() {
         super(EffectType.BENEFICIAL, 1919810);
+    }
+
+    @SubscribeEvent
+    public static void effects(LivingHurtEvent event) {
+        DamageSource source = event.getSource();
+        Effect effect_bless = EffectRegistry.BLESS_OF_DARK_ELF.get();
+        LivingEntity entity = event.getEntityLiving();
+
+        if(entity instanceof PlayerEntity && !entity.world.isRemote) {
+            PlayerEntity player = (PlayerEntity) entity;
+            if (player.isPotionActive(effect_bless)) {
+                int level = Objects.requireNonNull(player.getActivePotionEffect(effect_bless)).getAmplifier();
+                if (source.getDamageType().equals("inFire") || source.getDamageType().equals("onFire")
+                        || source.getDamageType().equals("lava")) {
+                    if (level >= 0) {
+                        event.setAmount(0);
+                    }
+                }else if (!source.isExplosion() || !source.isProjectile()) {
+                    if (level >= 10) {
+                        event.setAmount(0);
+                    } else {
+                        event.setAmount(event.getAmount() * (10 - level) / 10.0f);
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/tech/lq0/providencraft/effect/CurseOfSerpent.java
+++ src/main/java/tech/lq0/providencraft/effect/CurseOfSerpent.java
@@ -1,10 +1,37 @@
 package tech.lq0.providencraft.effect;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.providencraft.init.EffectRegistry;
 
+import java.util.Objects;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CurseOfSerpent extends Effect {
     public CurseOfSerpent() {
         super(EffectType.HARMFUL, 7324312);
+    }
+
+    @SubscribeEvent
+    public static void effects(LivingHurtEvent event) {
+        DamageSource source = event.getSource();
+        Effect effect_curse = EffectRegistry.CURSE_OF_SERPENT.get();
+        LivingEntity entity = event.getEntityLiving();
+
+        if(entity instanceof PlayerEntity && !entity.world.isRemote) {
+            PlayerEntity player = (PlayerEntity) entity;
+            if (player.isPotionActive(effect_curse)) {
+                int level = Objects.requireNonNull(player.getActivePotionEffect(effect_curse)).getAmplifier();
+                if (source.isExplosion()) {
+                    event.setAmount(event.getAmount() * (1 + level));
+                }
+            }
+        }
     }
 }
