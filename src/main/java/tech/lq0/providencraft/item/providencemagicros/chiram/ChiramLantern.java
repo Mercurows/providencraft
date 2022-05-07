66fc88f4f2d836488e7739f88fa8c33d85e16385
17146 <1714673995@qq.com>
2022-05-07 19:43:34 +0800

    第120次提交 添加了提灯功能材质 添加了云朵钥匙材质 提交了棉灰糖功能材质

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/chiram/ChiramLantern.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/chiram/ChiramLantern.java
@@ -1,10 +1,53 @@
 package tech.lq0.providencraft.item.providencemagicros.chiram;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class ChiramLantern extends Item {
     public ChiramLantern(){
-        super(new Properties().maxStackSize(1).group(ModGroup.itemgroup));
+        super(new Properties().maxStackSize(1).group(ModGroup.itemgroup).isImmuneToFire());
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("chiram_lantern_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("chiram_lantern_des2")).mergeStyle(TextFormatting.GRAY));
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.setFire(5);
+        return super.hitEntity(stack, target, attacker);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(entityIn instanceof PlayerEntity && isSelected){
+            PlayerEntity player = (PlayerEntity) entityIn;
+            if(worldIn.getLight(player.getPosition()) <= 5){
+                player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 300, 0));
+                player.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 300, 1));
+                player.addPotionEffect(new EffectInstance(Effects.SPEED, 300, 0));
+                player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 300, 0));
+            }
+        }
     }
 }
--- src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/Marshaimallow.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/Marshaimallow.java
@@ -1,13 +1,35 @@
 package tech.lq0.providencraft.item.providencemagicros.haine;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
 public class Marshaimallow extends Item {
-    private static final Food food = (new Food.Builder()).saturation(4.0f).hunger(5).build();
+    private static final Food food = (new Food.Builder()).saturation(4.0f).hunger(5).
+            effect(new EffectInstance(Effects.SLOW_FALLING, 600, 0), 1.0f).build();
 
     public Marshaimallow(){
         super(new Properties().food(food).group(ModGroup.itemgroup));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("marshaimallow_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("marshaimallow_des2")).mergeStyle(TextFormatting.GRAY));
     }
 }
