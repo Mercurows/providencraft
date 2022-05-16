53f3102668e4695e196fe51ceb74577444c4e4ec
17146 <1714673995@qq.com>
2022-05-16 23:51:03 +0800

    第125次提交 添加了魅魔之尾的材质和功能

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/hoshimiya/SuccubusTail.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/hoshimiya/SuccubusTail.java
@@ -1,13 +1,72 @@
 package tech.lq0.providencraft.item.providencemagicros.hoshimiya;
 
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.models.SuccubusTailModel;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class SuccubusTail extends ArmorItem {
     public SuccubusTail(){
         super(ArmorMaterial.CHAIN, EquipmentSlotType.LEGS, new Properties().defaultMaxDamage(530).setNoRepair().rarity(Rarity.RARE).group(ModGroup.itemgroup));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Nullable
+    @Override
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new SuccubusTailModel<>();
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return Utils.MOD_ID + ":textures/models/armor/succubus_tail_model_texture.png";
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("succubus_tail_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("succubus_tail_des2")).mergeStyle(TextFormatting.GRAY));
+    }
+
+    @SubscribeEvent
+    public static void tailEffect(LivingHurtEvent event) {
+        if (event.getEntityLiving() instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+            ItemStack tail = player.getItemStackFromSlot(EquipmentSlotType.LEGS);
+            if(!tail.isEmpty() && tail.getItem().equals(ItemRegistry.SUCCUBUS_TAIL.get())) {
+                int random = (int) (Math.random() * 10 + 1);
+                if (random > 7) {
+                    float damage = event.getAmount();
+                    player.heal(damage);
+                    event.setAmount(0.0f);
+                }
+            }
+        }
     }
 }
