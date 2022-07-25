7146b7386158d15e79b1e7d5313f53f63c90fc97
17146 <1714673995@qq.com>
2022-07-26 01:11:06 +0800

    第146次提交 重构了提灯

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/chiram/ChiramLantern.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/chiram/ChiramLantern.java
@@ -4,11 +4,14 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.Rarity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -21,10 +24,11 @@ import tech.lq0.providencraft.init.ItemRegistry;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.function.Consumer;
 
-public class ChiramLantern extends Item {
+public class ChiramLantern extends Item{
     public ChiramLantern(){
-        super(new Properties().maxStackSize(1).group(ModGroup.itemgroup).isImmuneToFire().rarity(Rarity.UNCOMMON));
+        super(new Properties().maxStackSize(1).group(ModGroup.itemgroup).isImmuneToFire().defaultMaxDamage(1031).rarity(Rarity.UNCOMMON));
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -36,24 +40,33 @@ public class ChiramLantern extends Item {
     }
 
     @Override
-    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        target.setFire(5);
-        target.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 100, 0));
-        return super.hitEntity(stack, target, attacker);
+    public boolean isEnchantable(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        return repair.getItem() == Items.TORCH;
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return false;
     }
 
     @Override
     public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         if(entityIn instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity) entityIn;
-            if (player.getHeldItemOffhand().getItem().equals(ItemRegistry.CHIRAM_LANTERN.get()) || player.getHeldItemMainhand().getItem().equals(ItemRegistry.CHIRAM_LANTERN.get())) {
-                if (worldIn.getLight(player.getPosition()) <= 5) {
-                    if(player.ticksExisted % 600 == 0) {
-                        player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 100, 0));
-                    }
+            if (player.getHeldItemOffhand().getItem().equals(ItemRegistry.CHIRAM_LANTERN.get())) {
+                ItemStack lantern = player.getHeldItemOffhand();
+                if (worldIn.getLight(player.getPosition()) <= 5 && lantern.getDamage() < lantern.getMaxDamage()) {
                     player.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 300, 1));
                     player.addPotionEffect(new EffectInstance(Effects.SPEED, 300, 0));
                     player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 300, 0));
+                    if(player.ticksExisted % 20 == 0 && !player.abilities.isCreativeMode) {
+                        lantern.setDamage(lantern.getDamage() + 1);
+                    }
                 }
             }
         }
