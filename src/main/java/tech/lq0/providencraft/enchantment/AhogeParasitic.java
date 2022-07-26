ad71f7847e5a72da196a4681409c2166eddbe5ad
17146 <1714673995@qq.com>
2022-07-27 01:36:59 +0800

    第148次提交 新增了一堆进度 重写了两个附魔

--- src/main/java/tech/lq0/providencraft/enchantment/AhogeParasitic.java
+++ src/main/java/tech/lq0/providencraft/enchantment/AhogeParasitic.java
@@ -1,9 +1,14 @@
 package tech.lq0.providencraft.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import tech.lq0.providencraft.init.EnchantRegistry;
 import tech.lq0.providencraft.init.ItemRegistry;
 
 public class AhogeParasitic extends Enchantment {
@@ -31,6 +36,26 @@ public class AhogeParasitic extends Enchantment {
             return true;
         } else {
             return super.canApply(stack);
+        }
+    }
+
+    @Override
+    public void onEntityDamaged(LivingEntity user, Entity target, int level) {
+        int times = 2;
+        if (user instanceof PlayerEntity && !user.world.isRemote() && !target.isAlive()) {
+            PlayerEntity player = (PlayerEntity) user;
+            ItemStack heldItem = player.getHeldItemMainhand();
+
+            ItemStack helmet = player.getItemStackFromSlot(EquipmentSlotType.HEAD);
+            if (!helmet.isEmpty() && helmet.getItem().equals(ItemRegistry.RED_AHOGE_HELMET.get())) {
+                times++;
+            }
+            if (heldItem.getItem().equals(ItemRegistry.RED_AHOGE_SWORD.get()) ||
+                    heldItem.getItem().equals(ItemRegistry.RED_AHOGE_BOOMERANG.get())) {
+                times++;
+            }
+
+            player.heal(level * times / 2.0f);
         }
     }
 }
--- src/main/java/tech/lq0/providencraft/enchantment/UniHusk.java
+++ src/main/java/tech/lq0/providencraft/enchantment/UniHusk.java
@@ -1,8 +1,16 @@
 package tech.lq0.providencraft.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import tech.lq0.providencraft.init.EnchantRegistry;
 
 public class UniHusk extends Enchantment {
     private static final EquipmentSlotType[] CHEST = new EquipmentSlotType[]{EquipmentSlotType.CHEST};
@@ -21,5 +29,18 @@ public class UniHusk extends Enchantment {
 
     public int getMaxLevel() {
         return 3;
+    }
+
+    @Override
+    public void onUserHurt(LivingEntity user, Entity attacker, int level) {
+        if (user instanceof PlayerEntity && !user.world.isRemote()) {
+            if (attacker instanceof LivingEntity) {
+                LivingEntity livingEntity = (LivingEntity) attacker;
+
+                if (level > 0) {
+                    livingEntity.addPotionEffect(new EffectInstance(Effects.POISON, 100, level + 1));
+                }
+            }
+        }
     }
 }
