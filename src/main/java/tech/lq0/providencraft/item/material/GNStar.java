838ff9259bfd5160dc1f9108c61874eeafa54ffa
17146 <1714673995@qq.com>
2022-09-04 14:45:24 +0800

    第199次提交 添加了GN之魂 GN之星的材质和合成表

--- src/main/java/tech/lq0/providencraft/item/material/GNStar.java
+++ src/main/java/tech/lq0/providencraft/item/material/GNStar.java
@@ -1,10 +1,35 @@
 package tech.lq0.providencraft.item.material;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class GNStar extends Item {
     public GNStar(){
-        super(new Properties().group(ModGroup.itemgroup));
+        super(new Properties().group(ModGroup.itemgroup).isImmuneToFire().rarity(Rarity.UNCOMMON));
+    }
+
+    @Override
+    public boolean hasEffect(ItemStack stack) {
+        return true;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("gn_star_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("gn_star_des2")).mergeStyle(TextFormatting.GRAY));
     }
 }
