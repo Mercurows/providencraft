94d1367ca272869dedc06ba02a60bd8447853f2e
17146 <1714673995@qq.com>
2022-09-08 12:43:41 +0800

    第204次提交 添加了露西耶耳羽

--- src/main/java/tech/lq0/providencraft/item/providencethird/leciel/LecielEarCoverts.java
+++ src/main/java/tech/lq0/providencraft/item/providencethird/leciel/LecielEarCoverts.java
@@ -1,12 +1,106 @@
 package tech.lq0.providencraft.item.providencethird.leciel;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.init.SoundRegistry;
+import tech.lq0.providencraft.models.LecielEarCovertsModel;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.UUID;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class LecielEarCoverts extends ArmorItem {
     public LecielEarCoverts(){
         super(ArmorMaterial.LEATHER, EquipmentSlotType.HEAD, new Properties().maxDamage(285).group(ModGroup.itemgroup));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Nullable
+    @Override
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new LecielEarCovertsModel<>();
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return Utils.MOD_ID + ":textures/models/armor/leciel_ear_coverts_texture.png";
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("leciel_ear_coverts_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("leciel_ear_coverts_des2")).mergeStyle(TextFormatting.GRAY).mergeStyle(TextFormatting.ITALIC));
+        tooltip.add((new TranslationTextComponent("description_third")).mergeStyle(TextFormatting.DARK_AQUA).mergeStyle(TextFormatting.BOLD));
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    @Nonnull
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(equipmentSlot);
+        UUID uuid = new UUID(ItemRegistry.LECIEL_EAR_COVERTS.hashCode() + equipmentSlot.toString().hashCode(), 0);
+        if (equipmentSlot == getEquipmentSlot()) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.LUCK,
+                    new AttributeModifier(uuid, "leciel ear coverts modifier", 2.0f, AttributeModifier.Operation.ADDITION));
+            map.put(Attributes.MOVEMENT_SPEED,
+                    new AttributeModifier(uuid, "leciel ear coverts modifier", 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+        }
+        return map;
+    }
+
+    @SubscribeEvent
+    public static void effect(AttackEntityEvent event){
+        Entity entity = event.getTarget();
+        PlayerEntity player = event.getPlayer();
+
+        ItemStack item = player.getItemStackFromSlot(EquipmentSlotType.HEAD);
+        if(item.getItem().equals(ItemRegistry.LECIEL_EAR_COVERTS.get()) && !player.world.isRemote){
+            if(entity instanceof LivingEntity){
+                LivingEntity livingEntity = (LivingEntity) entity;
+                if(livingEntity instanceof AnimalEntity || livingEntity instanceof VillagerEntity){
+                    player.addPotionEffect(new EffectInstance(Effects.WEAKNESS, 200, 0));
+                    player.addPotionEffect(new EffectInstance(Effects.UNLUCK, 200, 0));
+
+                    player.playSound(SoundRegistry.SORRY.get(), SoundCategory.AMBIENT, 0.5f,1.0f);
+                }
+
+            }
+
+        }
     }
 }
