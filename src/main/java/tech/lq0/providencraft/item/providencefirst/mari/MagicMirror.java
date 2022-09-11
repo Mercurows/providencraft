2ae602b7032e5b5bb81dd5b44be90e78d83d6926
17146 <1714673995@qq.com>
2022-09-11 18:01:36 +0800

    第209次提交 完成了魔镜和月光护符的功能 新建了yuri的文件夹

--- src/main/java/tech/lq0/providencraft/item/providencefirst/mari/MagicMirror.java
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/mari/MagicMirror.java
@@ -1,10 +1,68 @@
 package tech.lq0.providencraft.item.providencefirst.mari;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class MagicMirror extends Item {
     public MagicMirror(){
-        super(new Properties().group(ModGroup.itemgroup));
+        super(new Properties().group(ModGroup.itemgroup).maxDamage(136));
+    }
+
+    private final Effect[][] EFFECT = {
+            {Effects.STRENGTH, Effects.WEAKNESS},
+            {Effects.SPEED, Effects.SLOWNESS},
+            {Effects.SLOW_FALLING, Effects.LEVITATION},
+            {Effects.NIGHT_VISION, Effects.NAUSEA},
+            {Effects.HASTE, Effects.MINING_FATIGUE},
+            {Effects.SATURATION, Effects.HUNGER},
+            {Effects.INVISIBILITY, Effects.BLINDNESS},
+            {Effects.REGENERATION, Effects.WITHER}
+    };
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemStack = playerIn.getHeldItem(handIn);
+        for(Effect[] effect : EFFECT){
+            if(playerIn.isPotionActive(effect[0])){
+                playerIn.removePotionEffect(effect[0]);
+                playerIn.addPotionEffect(new EffectInstance(effect[1], 600, 1));
+            }else if(playerIn.isPotionActive(effect[1])){
+                playerIn.removePotionEffect(effect[1]);
+                playerIn.addPotionEffect(new EffectInstance(effect[0], 600, 1));
+            }
+        }
+        playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 100);
+        itemStack.damageItem(1, playerIn, (playerEntity) -> playerEntity.sendBreakAnimation(handIn));
+
+        return new ActionResult<>(ActionResultType.SUCCESS, itemStack);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("magic_mirror_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("magic_mirror_des2")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.MARI);
     }
 }
--- src/main/java/tech/lq0/providencraft/item/providencesecond/lucia/MoonlightAmulet.java
+++ src/main/java/tech/lq0/providencraft/item/providencesecond/lucia/MoonlightAmulet.java
@@ -1,10 +1,75 @@
 package tech.lq0.providencraft.item.providencesecond.lucia;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.init.EffectRegistry;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.tools.ItemNBTTool;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.UUID;
 
 public class MoonlightAmulet extends Item {
+    public static final String TAG_MOONLIGHT = "isnight";
+
     public MoonlightAmulet(){
         super(new Properties().group(ModGroup.itemgroup));
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(slot, stack);
+        UUID uuid = new UUID(ItemRegistry.MOONLIGHT_AMULET.hashCode() + slot.toString().hashCode(), 0);
+        if (slot == EquipmentSlotType.OFFHAND) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.MAX_HEALTH,
+                    new AttributeModifier(uuid, "moonlight amulet modifier", 6.0, AttributeModifier.Operation.ADDITION));
+            map.put(Attributes.ATTACK_DAMAGE,
+                    new AttributeModifier(uuid, "moonlight amulet modifier", 2.0, AttributeModifier.Operation.ADDITION));
+            map.put(Attributes.ARMOR,
+                    new AttributeModifier(uuid, "moonlight amulet modifier", 4.0, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(entityIn instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entityIn;
+            if (!worldIn.isDaytime()) {
+                ItemNBTTool.setBoolean(stack, TAG_MOONLIGHT, true);
+                player.addPotionEffect(new EffectInstance(EffectRegistry.BIG_FIERCE_ONE.get(), 300, 0));
+            }else {
+                ItemNBTTool.setBoolean(stack, TAG_MOONLIGHT, false);
+            }
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("moonlight_amulet_des")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.LUCIA);
     }
 }
