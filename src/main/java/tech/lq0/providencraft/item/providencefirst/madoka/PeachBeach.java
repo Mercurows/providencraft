1d36b8d59ddc3e81f705ad485e6155388e6c5302
17146 <1714673995@qq.com>
2022-09-12 15:56:36 +0800

    第210次提交 做完了性感沙滩 添加了月光护符的材质

--- src/main/java/tech/lq0/providencraft/item/providencefirst/madoka/PeachBeach.java
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/madoka/PeachBeach.java
@@ -1,10 +1,48 @@
 package tech.lq0.providencraft.item.providencefirst.madoka;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class PeachBeach extends Item {
+    private static final Food food = (new Food.Builder()).saturation(5).hunger(4).setAlwaysEdible().
+            effect(new EffectInstance(Effects.NAUSEA, 300, 0), 0.3f).
+            effect(new EffectInstance(Effects.STRENGTH, 300, 0), 1.0f).build();
+
     public PeachBeach(){
-        super(new Properties().group(ModGroup.itemgroup));
+        super(new Properties().group(ModGroup.itemgroup).food(food).maxStackSize(1));
+    }
+
+    @Override
+    @Nonnull
+    public SoundEvent getEatSound() {
+        return SoundEvents.ENTITY_GENERIC_DRINK;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("peach_beach_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("peach_beach_des2")).mergeStyle(TextFormatting.GRAY).mergeStyle(TextFormatting.ITALIC));
+        TooltipTool.addLiverInfo(tooltip, Livers.MADOKA);
     }
 }
