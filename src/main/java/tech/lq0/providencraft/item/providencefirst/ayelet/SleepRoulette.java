37ac13c6e61ccea2eb328cdc149620745ef4a0eb
17146 <1714673995@qq.com>
2022-09-13 12:20:43 +0800

    第211次提交 添加了月光护符和黑听耳机的合成表 注册了睡眠轮盘

---
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/ayelet/SleepRoulette.java
@@ -0,0 +1,10 @@
+package tech.lq0.providencraft.item.providencefirst.ayelet;
+
+import net.minecraft.item.Item;
+import tech.lq0.providencraft.group.ModGroup;
+
+public class SleepRoulette extends Item {
+    public SleepRoulette(){
+        super(new Properties().group(ModGroup.itemgroup));
+    }
+}
