d8875e0a44973fdfe204a70ecf90c3f2a7807c3c
17146 <1714673995@qq.com>
2022-10-01 01:15:50 +0800

    第243次提交 添加了砂糖相机的模型

--- src/main/java/tech/lq0/providencraft/item/providencesecond/satou/SatouCamera.java
+++ src/main/java/tech/lq0/providencraft/item/providencesecond/satou/SatouCamera.java
@@ -1,10 +1,32 @@
 package tech.lq0.providencraft.item.providencesecond.satou;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class SatouCamera extends Item {
     public SatouCamera(){
-        super(new Properties().group(ModGroup.itemgroup).maxStackSize(1));
+        super(new Properties().group(ModGroup.itemgroup).maxStackSize(1).maxDamage(48));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("satou_camera_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("satou_camera_des2")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.SATOU);
     }
 }
