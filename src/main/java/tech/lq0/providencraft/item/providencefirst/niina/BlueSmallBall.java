fdacedece19d260439909624338359e56a99ac73
17146 <1714673995@qq.com>
2022-10-02 22:33:11 +0800

    第248次提交 修改了蓝色小球的描述和价格

--- src/main/java/tech/lq0/providencraft/item/providencefirst/niina/BlueSmallBall.java
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/niina/BlueSmallBall.java
@@ -31,7 +31,8 @@ public class BlueSmallBall extends Item {
     @OnlyIn(Dist.CLIENT)
     @ParametersAreNonnullByDefault
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        tooltip.add((new TranslationTextComponent("blue_small_ball_des")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("blue_small_ball_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("blue_small_ball_des2")).mergeStyle(TextFormatting.GRAY));
         TooltipTool.addLiverInfo(tooltip, Livers.NIINA);
     }
 
