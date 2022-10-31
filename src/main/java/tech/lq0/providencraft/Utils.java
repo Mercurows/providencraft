c66b05711e1b7dc2dbe9b09d46c3355bbb01792f
Light_Quanta <ZCH22248@foxmail.com>
2022-10-31 23:38:00 +0800

    移除部分无用代码

--- src/main/java/tech/lq0/providencraft/Utils.java
+++ src/main/java/tech/lq0/providencraft/Utils.java
@@ -5,27 +5,19 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import tech.lq0.providencraft.init.*;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("providencraft")
 public class Utils {
-    // Directly reference a log4j logger.
-    //private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "providencraft";
 
     public Utils() {
-       // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-       // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
         ItemRegistry.ITEMS.register(eventBus);
@@ -43,38 +35,11 @@ public class Utils {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        //LOGGER.info("HELLO FROM PREINIT");
-        //LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        //InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        //LOGGER.info("Got IMC {}", event.getIMCStream().
-        //        map(m->m.messageSupplier().get()).
-        //        collect(Collectors.toList()));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        //LOGGER.info("HELLO from server starting");
-    }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-//    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-//    public static class RegistryEvents {
-//        @SubscribeEvent
-//        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-//            // register a new block here
-//            //LOGGER.info("HELLO from Register Block");
-//        }
-//    }
+    }
 }
