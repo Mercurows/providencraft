fd811f6a53b927177a9de4a94f767bcfce6486c5
17146 <1714673995@qq.com>
2023-01-26 00:13:42 +0800

    添加了深水鱼 第334次提交

---
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/fukami/FukamizuFish.java
@@ -0,0 +1,37 @@
+package tech.lq0.providencraft.item.providenceOI.fukami;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class FukamizuFish extends Item {
+    private static final Food food = (new Food.Builder()).saturation(6.0f).hunger(6).
+            effect(() -> new EffectInstance(Effects.LUCK, 600, 0), 1.0f).build();
+
+    public FukamizuFish(){
+        super(new Properties().group(ModGroup.itemgroup).food(food));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("fukamizu_fish_des1").mergeStyle(TextFormatting.GRAY));
+        tooltip.add(new TranslationTextComponent("fukamizu_fish_des2").mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.FUKAMI);
+    }
+}
