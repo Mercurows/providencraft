656c7e87143ba42274aae95d21d22067088d587c
17146 <1714673995@qq.com>
2023-01-29 02:25:50 +0800

    添加了旧毛毛刀来防止物品失效 第345次提交

---
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/shirako/MomoKnife.java
@@ -0,0 +1,55 @@
+package tech.lq0.providencraft.item.providenceOI.shirako;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.SwordItem;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import tech.lq0.providencraft.init.ItemRegistry;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class MomoKnife extends SwordItem {
+    /**
+    * Momo Knife has been replaced by Momo Cutter.
+    * This item is designed to replace the knife to the cutter.
+    * */
+
+    public MomoKnife(){
+        super(ItemTier.IRON, 1, -1.0f, new Properties().maxDamage(1231).rarity(Rarity.EPIC));
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(entityIn instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) entityIn;
+
+            CompoundNBT nbt = stack.getTag();
+
+            ItemStack newStack = ItemRegistry.MOMO_CUTTER.get().getDefaultInstance();
+            newStack.setTag(nbt);
+
+            player.inventory.setInventorySlotContents(player.inventory.getSlotFor(stack), newStack);
+        }
+        super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("momo_old_knife_des")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.SHIRAKO);
+    }
+}
