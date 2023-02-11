5953c1d07c85a10a3eb14eb1cf5c6fb7c3c77aef
17146 <1714673995@qq.com>
2023-02-11 22:44:09 +0800

    添加了棉灰糖玩偶的描述 第391次提交

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/CursedMarshaimallow.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/CursedMarshaimallow.java
@@ -1,5 +1,6 @@
 package tech.lq0.providencraft.item.providencemagicros.haine;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -7,9 +8,20 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.entity.CursedCatDollEntity;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class CursedMarshaimallow extends Item {
     public CursedMarshaimallow(){
@@ -42,6 +54,14 @@ public class CursedMarshaimallow extends Item {
                 }
             }
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("cursed_marshaimallow_des1")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add((new TranslationTextComponent("cursed_marshaimallow_des2")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.HAINE);
     }
 
     @Override
