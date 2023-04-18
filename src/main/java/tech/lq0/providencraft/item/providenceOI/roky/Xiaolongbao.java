f24032377e18fb12487514354e55cbbb0fd8fd03
17146 <1714673995@qq.com>
2023-04-18 15:34:12 +0800

    添加了楽烯小笼包的功能 添加了月蚀暗夜buff图标

--- src/main/java/tech/lq0/providencraft/item/providenceOI/roky/Xiaolongbao.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/roky/Xiaolongbao.java
@@ -1,13 +1,20 @@
 package tech.lq0.providencraft.item.providenceOI.roky;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SExplosionPacket;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
@@ -36,5 +43,30 @@ public class Xiaolongbao extends Item {
     @Override
     public int getUseDuration(ItemStack stack) {
         return 50;
+    }
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        ItemStack itemStack = super.onItemUseFinish(stack, worldIn, entityLiving);
+        if (entityLiving instanceof PlayerEntity && !worldIn.isRemote) {
+            PlayerEntity player = (PlayerEntity) entityLiving;
+            double random = Math.random();
+            if (random < .05) {
+                Explosion explosion = new Explosion(worldIn, player, DamageSource.causeExplosionDamage(player),
+                        null, player.getPosX(), player.getPosY(), player.getPosZ(), 2, false, Explosion.Mode.NONE);
+                explosion.doExplosionA();
+                explosion.doExplosionB(true);
+
+                explosion.clearAffectedBlockPositions();
+
+                for(ServerPlayerEntity serverPlayer : ((ServerWorld) worldIn).getPlayers()) {
+                    if(serverPlayer.getDistanceSq(player.getPosX(), player.getPosY(), player.getPosZ()) < 100) {
+                        serverPlayer.connection.sendPacket(new SExplosionPacket(player.getPosX(), player.getPosY(), player.getPosZ(), 2, explosion.getAffectedBlockPositions(), explosion.getPlayerKnockbackMap().get(player)));
+                    }
+                }
+            }
+
+        }
+        return itemStack;
     }
 }
