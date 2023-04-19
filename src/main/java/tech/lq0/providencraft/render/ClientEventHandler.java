4afd1a6aa1cf0d779a151ecdeeaddb4fb4ba6167
时雨 <12388133+Jacksigxu@user.noreply.gitee.com>
2023-04-19 10:02:33 +0000

    修复原野木树苗渲染问题
    Signed-off-by: 时雨 <12388133+Jacksigxu@user.noreply.gitee.com>

--- src/main/java/tech/lq0/providencraft/render/ClientEventHandler.java
+++ src/main/java/tech/lq0/providencraft/render/ClientEventHandler.java
@@ -1,11 +1,14 @@
 package tech.lq0.providencraft.render;
 
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import tech.lq0.providencraft.init.BlockRegistry;
 import tech.lq0.providencraft.init.EntityRegistry;
 import tech.lq0.providencraft.init.TileEntityRegistry;
 
@@ -27,5 +30,6 @@ public class ClientEventHandler {
         RenderingRegistry.registerEntityRenderingHandler(EntityRegistry.CURSED_CAT_DOLL_ENTITY.get(), CursedCatDollEntityRenderer::new);
 
         event.enqueueWork(() -> ClientRegistry.bindTileEntityRenderer(TileEntityRegistry.POINTS_STORE.get(), PointsStoreTileEntityRenderer::new));
+        event.enqueueWork(() -> {RenderTypeLookup.setRenderLayer(BlockRegistry.HARANO_SAPLING.get(), RenderType.getCutout());});
     }
 }
