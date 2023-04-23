7f93f1295a8d069bbd807b0dbde5293f6d7521b0
17146 <1714673995@qq.com>
2023-04-23 22:31:18 +0800

    添加了月蚀长剑的描述

--- src/main/java/tech/lq0/providencraft/item/providenceOI/sorayo/LunarEclipse.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/sorayo/LunarEclipse.java
@@ -1,5 +1,6 @@
 package tech.lq0.providencraft.item.providenceOI.sorayo;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ArmorStandEntity;
@@ -9,16 +10,33 @@ import net.minecraft.item.ItemTier;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.SwordItem;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.init.DamageSourceRegistry;
 import tech.lq0.providencraft.init.EffectRegistry;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 import java.util.Objects;
 
 public class LunarEclipse extends SwordItem {
     public LunarEclipse(){
         super(ItemTier.NETHERITE, 2, -2.3f, new Properties().group(ModGroup.itemgroup).rarity(Rarity.EPIC).maxDamage(928));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("des.providencraft.lunar_eclipse")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.SORAYO);
     }
 
     @Override
