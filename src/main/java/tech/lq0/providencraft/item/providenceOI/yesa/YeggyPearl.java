511e4b0268b7eee2fbd677957f29b929d4677b89
17146 <1714673995@qq.com>
2023-04-30 23:11:14 +0800

    添加了伊头珠的HUD

--- src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
@@ -14,7 +14,6 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
