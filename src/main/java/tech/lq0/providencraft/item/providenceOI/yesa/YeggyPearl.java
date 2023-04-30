53960aaaa77723267650fb1f9783af90c9a11ea7
17146 <1714673995@qq.com>
2023-04-30 18:14:22 +0800

    添加了伊头珠的贴图

--- src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
@@ -93,8 +93,10 @@ public class YeggyPearl extends ArmorItem {
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
         setEnergy(stack, getEnergy(stack) + 1);
 
-        if(getEnergy(stack) == 40){
-            player.playSound(SoundEvents.ENTITY_ARROW_HIT_PLAYER, 0.5F, 1.0F);
+        if(world.isRemote) {
+            if (getEnergy(stack) == 40) {
+                player.playSound(SoundEvents.ENTITY_ARROW_HIT_PLAYER, 0.5F, 1.0F);
+            }
         }
 
         super.onArmorTick(stack, world, player);
