6258effc08a743deddd983271ddc5eaea0a06637
17146 <1714673995@qq.com>
2023-05-18 23:44:48 +0800

    添加了原野木群系的建筑

--- src/main/java/tech/lq0/providencraft/event/EventHandler.java
+++ src/main/java/tech/lq0/providencraft/event/EventHandler.java
@@ -5,7 +5,9 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tech.lq0.providencraft.world.biome.BlueDesertBiome;
+import tech.lq0.providencraft.world.biome.HaranoPlainsBiome;
 import tech.lq0.providencraft.world.feature.structure.BlueDesertStructures;
+import tech.lq0.providencraft.world.feature.structure.HaranoPlainsStructures;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
@@ -14,6 +16,9 @@ public class EventHandler {
         if(event.getName().equals(BlueDesertBiome.blueDesertBiome.getRegistryName())){
             event.getGeneration().getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES)
                     .add(() -> BlueDesertStructures.BLUE_DESERT_WELL_FEATURE);
+        }else if(event.getName().equals(HaranoPlainsBiome.haranoPlainsBiome.getRegistryName())){
+            event.getGeneration().getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES)
+                    .add(() -> HaranoPlainsStructures.HARANO_TENT_FEATURE);
         }
     }
 }
--- src/main/java/tech/lq0/providencraft/event/ModEventHandler.java
+++ src/main/java/tech/lq0/providencraft/event/ModEventHandler.java
@@ -9,12 +9,15 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.world.feature.structure.BlueDesertStructures;
+import tech.lq0.providencraft.world.feature.structure.HaranoPlainsStructures;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventHandler {
     @SubscribeEvent
     public static void structureRegistry(final RegistryEvent.Register<Feature<?>> event) {
         event.getRegistry().register(BlueDesertStructures.BLUE_DESERT_WELL.setRegistryName(Utils.MOD_ID, "blue_desert_well"));
+        event.getRegistry().register(HaranoPlainsStructures.HARANO_TENT.setRegistryName(Utils.MOD_ID, "harano_tent"));
         Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(Utils.MOD_ID, "blue_desert_well"), BlueDesertStructures.BLUE_DESERT_WELL_FEATURE);
+        Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation(Utils.MOD_ID, "harano_tent"), HaranoPlainsStructures.HARANO_TENT_FEATURE);
     }
 }
