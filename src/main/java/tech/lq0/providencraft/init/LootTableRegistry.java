959105daf7ca64892a06a99be63aea834423025c
17146 <1714673995@qq.com>
2023-05-24 22:45:46 +0800

    白雪葵的唱片可以生成了

--- src/main/java/tech/lq0/providencraft/init/LootTableRegistry.java
+++ src/main/java/tech/lq0/providencraft/init/LootTableRegistry.java
@@ -13,6 +13,7 @@ import java.util.Map;
 public class LootTableRegistry {
     public static final ResourceLocation PDC_CHEST = new ResourceLocation(Utils.MOD_ID, "chests/pdc_chest");
     public static final ResourceLocation PDC_DISC_CHEST = new ResourceLocation(Utils.MOD_ID, "chests/pdc_disc_chest");
+    public static final ResourceLocation PDC_IGLOO_CHEST = new ResourceLocation(Utils.MOD_ID, "chests/pdc_igloo_chest");
 
     private static LootTableRegistry registry;
 
@@ -27,6 +28,7 @@ public class LootTableRegistry {
         lootTables.put(LootTables.CHESTS_DESERT_PYRAMID, PDC_CHEST);
         lootTables.put(LootTables.CHESTS_ABANDONED_MINESHAFT, PDC_CHEST);
         lootTables.put(LootTables.CHESTS_END_CITY_TREASURE, PDC_DISC_CHEST);
+        lootTables.put(LootTables.CHESTS_IGLOO_CHEST, PDC_IGLOO_CHEST);
     }
 
     public static LootTableRegistry getRegistry(){
