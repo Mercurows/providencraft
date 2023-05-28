b39f6413717d883ad6f37929c42acb132251657e
17146 <1714673995@qq.com>
2023-05-28 15:39:46 +0800

    添加了eli牌子的攻击动作

--- src/main/java/tech/lq0/providencraft/item/providencesecond/louise/UniMillet.java
+++ src/main/java/tech/lq0/providencraft/item/providencesecond/louise/UniMillet.java
@@ -2,7 +2,10 @@ package tech.lq0.providencraft.item.providencesecond.louise;
 
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.BlockNamedItem;
+import net.minecraft.item.Food;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -19,8 +22,11 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
 public class UniMillet extends BlockNamedItem {
+    private static final Food food = (new Food.Builder()).saturation(0.25f).hunger(2).fastToEat()
+            .effect(() -> new EffectInstance(Effects.SPEED, 100, 0), 1.0f).build();
+
     public UniMillet(){
-        super(BlockRegistry.UNI_MILLET_BLOCK.get(), new Properties().group(ModGroup.itemgroup));
+        super(BlockRegistry.UNI_MILLET_BLOCK.get(), new Properties().group(ModGroup.itemgroup).food(food));
     }
 
     @OnlyIn(Dist.CLIENT)
