077fa8a08588be1ea5cef8704272f5708d444c1a
17146 <1714673995@qq.com>
2023-06-09 12:59:03 +0800

    调整了新护甲的属性

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/MistyChestplate.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/MistyChestplate.java
@@ -2,23 +2,40 @@ package tech.lq0.providencraft.item.providencemagicros.haine;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.init.ItemRegistry;
 import tech.lq0.providencraft.tiers.ModArmorMaterial;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 import java.util.UUID;
 
 public class MistyChestplate extends ArmorItem {
     public MistyChestplate(){
         super(ModArmorMaterial.MAGICROS, EquipmentSlotType.CHEST, new Properties().group(ModGroup.itemgroup).isImmuneToFire().setNoRepair()
                 .rarity(Rarity.create("PROVIDENCRAFT_LEGENDARY", TextFormatting.GOLD)));
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        TooltipTool.addDevelopingText(tooltip);
+
+        TooltipTool.addLiverInfo(tooltip, Livers.HAINE);
     }
 
     @Override
--- src/main/java/tech/lq0/providencraft/item/providencemagicros/keroro/FroggyLeggings.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/keroro/FroggyLeggings.java
@@ -2,23 +2,41 @@ package tech.lq0.providencraft.item.providencemagicros.keroro;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeMod;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.init.ItemRegistry;
 import tech.lq0.providencraft.tiers.ModArmorMaterial;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 import java.util.UUID;
 
 public class FroggyLeggings extends ArmorItem {
     public FroggyLeggings(){
         super(ModArmorMaterial.MAGICROS, EquipmentSlotType.LEGS, new Properties().group(ModGroup.itemgroup).isImmuneToFire().setNoRepair()
                 .rarity(Rarity.create("PROVIDENCRAFT_LEGENDARY", TextFormatting.GOLD)));
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        TooltipTool.addDevelopingText(tooltip);
+
+        TooltipTool.addLiverInfo(tooltip, Livers.KERORO);
     }
 
     @Override
@@ -29,6 +47,8 @@ public class FroggyLeggings extends ArmorItem {
             map = HashMultimap.create(map);
             map.put(Attributes.ARMOR_TOUGHNESS,
                     new AttributeModifier(uuid, "froggy leggings modifier", 6.0f, AttributeModifier.Operation.ADDITION));
+            map.put(ForgeMod.SWIM_SPEED.get(),
+                    new AttributeModifier(uuid, "froggy leggings modifier", 0.4f, AttributeModifier.Operation.MULTIPLY_BASE));
         }
         return map;
     }
