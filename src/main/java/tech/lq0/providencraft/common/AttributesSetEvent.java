856b0970326833bcdbbaf7c3d9df573680882211
时雨 <12388133+Jacksigxu@user.noreply.gitee.com>
2023-06-15 16:24:47 +0000

    修复了Attribute的添加
    Signed-off-by: 时雨 <12388133+Jacksigxu@user.noreply.gitee.com>

--- src/main/java/tech/lq0/providencraft/common/AttributesSetEvent.java
+++ src/main/java/tech/lq0/providencraft/common/AttributesSetEvent.java
@@ -1,20 +1,17 @@
 package tech.lq0.providencraft.common;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import tech.lq0.providencraft.init.AttributeRegistry;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AttributesSetEvent {
     @SubscribeEvent
-    public static void setupAttributes(FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-                GlobalEntityTypeAttributes.put(EntityType.PLAYER, PlayerEntity.func_234570_el_().createMutableAttribute(AttributeRegistry.CHAOS.get(), 0.0D).create());
-            }
-        );
+    public static void createAttributes(EntityAttributeModificationEvent event){
+        if (!event.has(EntityType.PLAYER, AttributeRegistry.CHAOS.get())) {
+            event.add(EntityType.PLAYER, AttributeRegistry.CHAOS.get());
+        }
     }
 }
