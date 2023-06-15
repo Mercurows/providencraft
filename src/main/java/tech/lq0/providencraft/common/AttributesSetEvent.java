f1d328b911404ffaa31739b6d09d5b2a23922e59
时雨 <12388133+Jacksigxu@user.noreply.gitee.com>
2023-06-15 15:13:49 +0000

    回滚Attribute注册
    Signed-off-by: 时雨 <12388133+Jacksigxu@user.noreply.gitee.com>

--- src/main/java/tech/lq0/providencraft/common/AttributesSetEvent.java
+++ src/main/java/tech/lq0/providencraft/common/AttributesSetEvent.java
@@ -1,16 +1,20 @@
 package tech.lq0.providencraft.common;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import tech.lq0.providencraft.init.AttributeRegistry;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AttributesSetEvent {
     @SubscribeEvent
-    public static void setupAttributes(EntityAttributeCreationEvent event) {
-        event.put(EntityType.PLAYER, PlayerEntity.func_234570_el_().createMutableAttribute(AttributeRegistry.CHAOS.get(), 0.0D).create());
+    public static void setupAttributes(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+                GlobalEntityTypeAttributes.put(EntityType.PLAYER, PlayerEntity.func_234570_el_().createMutableAttribute(AttributeRegistry.CHAOS.get(), 0.0D).create());
+            }
+        );
     }
 }
