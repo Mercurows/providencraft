7c9ed5ba2659a4f1140b6676550110d1d336f505
17146 <1714673995@qq.com>
2023-06-18 22:26:53 +0800

    添加了混沌检测仪的材质

--- src/main/java/tech/lq0/providencraft/item/others/ChaosChecker.java
+++ src/main/java/tech/lq0/providencraft/item/others/ChaosChecker.java
@@ -1,14 +1,23 @@
 package tech.lq0.providencraft.item.others;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.capability.ChaosHelper;
 import tech.lq0.providencraft.group.ModGroup;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ChaosChecker extends Item {
     public ChaosChecker(){
@@ -22,5 +31,12 @@ public class ChaosChecker extends Item {
             playerIn.sendStatusMessage(new StringTextComponent("混沌值为" + chaos), true);
         }
         return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("des.providencraft.chaos_checker_1").mergeStyle(TextFormatting.GRAY));
+        tooltip.add(new TranslationTextComponent("des.providencraft.chaos_checker_2").mergeStyle(TextFormatting.GRAY).mergeStyle(TextFormatting.ITALIC));
     }
 }
