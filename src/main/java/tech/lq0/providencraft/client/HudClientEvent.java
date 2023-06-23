f1f623139a59845d16f6badf850dd83d0ff2eb04
Light_Quanta <ZCH22248@foxmail.com>
2023-06-24 02:23:53 +0800

    重写混沌检测器UI渲染方式

--- src/main/java/tech/lq0/providencraft/client/HudClientEvent.java
+++ src/main/java/tech/lq0/providencraft/client/HudClientEvent.java
@@ -1,6 +1,7 @@
 package tech.lq0.providencraft.client;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,12 +14,16 @@ import tech.lq0.providencraft.gui.EclipseNightHUD;
 import tech.lq0.providencraft.gui.EclipseNightHUD2;
 import tech.lq0.providencraft.gui.RainyButterflyHUD;
 import tech.lq0.providencraft.init.EffectRegistry;
+import tech.lq0.providencraft.init.ItemRegistry;
 import tech.lq0.providencraft.integration.CompatHandler;
 import tech.lq0.providencraft.integration.vrc.VirtuaRealCraftRegistry;
 import tech.lq0.providencraft.item.others.ChaosChecker;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class HudClientEvent {
+    public static final ChaosCheckerHUD CheckerHUD = new ChaosCheckerHUD();
+    private static int lastTick = 0;
+
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void onOverlayRender(RenderGameOverlayEvent.Post event) {
         if (event.getType() != RenderGameOverlayEvent.ElementType.HELMET) {
@@ -28,7 +33,7 @@ public class HudClientEvent {
             return;
         }
         PlayerEntity player = Minecraft.getInstance().player;
-        if(Minecraft.getInstance().player.isPotionActive(EffectRegistry.ECLIPSE_NIGHT.get())) {
+        if (Minecraft.getInstance().player.isPotionActive(EffectRegistry.ECLIPSE_NIGHT.get())) {
             int level = Math.min(5, player.getActivePotionEffect(EffectRegistry.ECLIPSE_NIGHT.get()).getAmplifier() + 1);
 
             EclipseNightHUD2 eclipseNightHUD2 = new EclipseNightHUD2(event.getMatrixStack(), level);
@@ -41,7 +46,7 @@ public class HudClientEvent {
 
     @SubscribeEvent
     public static void onRainyButterflyHudRender(RenderGameOverlayEvent.Post event) {
-        if(CompatHandler.virtuarealcraft) {
+        if (CompatHandler.virtuarealcraft) {
             if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) {
                 return;
             }
@@ -51,7 +56,7 @@ public class HudClientEvent {
 
             PlayerEntity player = Minecraft.getInstance().player;
 
-            if(player.isSpectator() || player.isCreative()){
+            if (player.isSpectator() || player.isCreative()) {
                 return;
             }
 
@@ -59,17 +64,17 @@ public class HudClientEvent {
             ItemStack mainhandStack = player.getHeldItemMainhand();
             ItemStack offhandStack = player.getHeldItemOffhand();
 
-            if(mainhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()){
+            if (mainhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()) {
                 stack = mainhandStack;
-            }else if(offhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()){
+            } else if (offhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()) {
                 stack = offhandStack;
             }
-            if(mainhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get() &&
-                    offhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()){
+            if (mainhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get() &&
+                    offhandStack.getItem() == VirtuaRealCraftRegistry.RAINY_BUTTERFLY.get()) {
                 stack = mainhandStack;
             }
 
-            if(stack != null) {
+            if (stack != null) {
                 RainyButterflyHUD butterflyHUD = new RainyButterflyHUD(event.getMatrixStack(), stack, player.world.isRaining());
                 butterflyHUD.render();
             }
@@ -82,24 +87,35 @@ public class HudClientEvent {
             return;
         }
 
-        PlayerEntity player = Minecraft.getInstance().player;
+        ClientPlayerEntity player = Minecraft.getInstance().player;
         assert player != null;
-        if(player.isSpectator()){
+        if (player.isSpectator()) {
             return;
         }
 
         ItemStack mainStack = player.getHeldItemMainhand();
         ItemStack offStack = player.getHeldItemOffhand();
-        if(!(mainStack.getItem() instanceof ChaosChecker) && offStack.getItem() instanceof ChaosChecker){
+        if (!(mainStack.getItem() instanceof ChaosChecker) && offStack.getItem() instanceof ChaosChecker) {
             mainStack = offStack;
         }
+        // TODO 获取混沌值
+        int chaos = ChaosChecker.getChaos(mainStack);
+//        System.out.println(chaos);
 
-        if(mainStack.getItem() instanceof ChaosChecker) {
-            int chaos = ChaosChecker.getChaos(mainStack);
-
-            ChaosCheckerHUD checkerHUD = new ChaosCheckerHUD(event.getMatrixStack(), chaos);
-            checkerHUD.render();
+        int status;
+        if (mainStack.getItem() instanceof ChaosChecker) {
+            // 主/副手持有检测器
+            status = 2;
+        } else if (player.inventory.hasItemStack(ItemRegistry.CHAOS_CHECKER.get().getDefaultInstance())) {
+            // 背包里有检测器
+            status = 1;
+        } else {
+            // 无检测器
+            status = 0;
         }
+        int tick = Minecraft.getInstance().player.ticksExisted;
+        CheckerHUD.render(event.getMatrixStack(), tick % 200 - 100, status, event.getPartialTicks(), tick == lastTick);
+        lastTick = tick;
 
     }
 }
--- src/main/java/tech/lq0/providencraft/gui/ChaosCheckerHUD.java
+++ src/main/java/tech/lq0/providencraft/gui/ChaosCheckerHUD.java
@@ -1,6 +1,7 @@
 package tech.lq0.providencraft.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
@@ -8,35 +9,141 @@ import net.minecraft.util.ResourceLocation;
 import tech.lq0.providencraft.Utils;
 
 public class ChaosCheckerHUD extends AbstractGui {
-    private final int width;
-    private final int height;
-    private final Minecraft minecraft;
+    private int width;
+    private int height;
+    private Minecraft minecraft;
     private final ResourceLocation HUD = new ResourceLocation(Utils.MOD_ID, "textures/gui/chaos_value.png");
-    private MatrixStack matrixStack;
-    private final int chaos;
+    private final ResourceLocation BAR = new ResourceLocation(Utils.MOD_ID, "textures/gui/chaos_value_bar.png");
+    private int lastStatus = 0;
+    private float progress = 0;
+    private final float maxProgress = 40;
+    private int lastX = 0;
+    private int lastY = 0;
+    private int lastW = 0;
+    private int lastH = 0;
+    private float lastAlpha = 0;
+    private boolean is0to1 = false;
 
-    public ChaosCheckerHUD(MatrixStack matrixStack, int chaos) {
-        this.width = Minecraft.getInstance().getMainWindow().getScaledWidth() / 2;
-        this.height = Minecraft.getInstance().getMainWindow().getScaledHeight() / 2;
+    public ChaosCheckerHUD() {
         this.minecraft = Minecraft.getInstance();
-        this.matrixStack = matrixStack;
-        this.chaos = chaos;
     }
 
-    public void setMatrixStack(MatrixStack stack) {
-        this.matrixStack = stack;
-    }
+    public void render(MatrixStack matrixStack, int chaos, int status, float partialTick, boolean addTick) {
 
-    public void render() {
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.getTextureManager().bindTexture(HUD);
-        blit(matrixStack, width - 101, height + 10, -0.5f, 0, 200, 32, 200, 32);
-        this.minecraft.getTextureManager().bindTexture(new ResourceLocation(Utils.MOD_ID, "textures/gui/chaos_value_bar.png"));
+        width = minecraft.getMainWindow().getScaledWidth() / 2;
+        height = minecraft.getMainWindow().getScaledHeight() / 2;
 
-        if (chaos > 0) {
-            blit(matrixStack, width - 66 + 60 - 60 * chaos / 100, height + 23, 60 - 60 * chaos / 100, 1, 60 * chaos / 100, 6, 60, 15);
-        } else if (chaos < 0) {
-            blit(matrixStack, width + 6, height + 23, 0.5f, 8, 60 * chaos / -100, 6, 60, 15);
+        if (addTick) progress = Math.min(maxProgress, progress + 1);
+
+        float alpha;
+        if (status == 1) {
+            alpha = 0.5f;
+        } else if (status == 2) {
+            alpha = 1;
+        } else {
+            alpha = 0;
         }
+        if (status != lastStatus) {
+            progress = 0;
+            if (lastStatus != 2) {
+                lastW = 100;
+                lastH = 16;
+                lastX = width - 50;
+                lastY = 0;
+                lastAlpha = lastStatus == 1 ? 0.5f : 0;
+            } else {
+                lastW = 200;
+                lastH = 32;
+                lastX = width - 101;
+                lastY = height + 10;
+                lastAlpha = 1;
+            }
+            if (lastStatus == 0 && status == 1) is0to1 = true;
+            lastStatus = status;
+        }
+
+        float rate;
+        if (progress == maxProgress) {
+            rate = 1;
+            is0to1 = false;
+        } else {
+            progress = (int) progress + partialTick;
+            rate = (float) Math.sqrt(Math.sqrt(progress / maxProgress));
+        }
+
+        // 设置透明度
+        // TODO 修复透明度无效的Bug
+        RenderSystem.disableDepthTest();
+        RenderSystem.depthMask(false);
+        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+                GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, ease(lastAlpha, alpha, rate));
+        RenderSystem.disableAlphaTest();
+
+        // 渲染外框
+        this.minecraft.getTextureManager().bindTexture(HUD);
+        if (status == 2) {
+            blit(matrixStack, ease(lastX, width - 101, rate), ease(lastY, height + 10, rate), ease(lastW, 200, rate), ease(lastH, 32, rate), -0.5f, 0, 200, 32, 200, 32);
+        } else {
+            blit(matrixStack, ease(lastX, width - 50, rate), ease(lastY, 0, rate), ease(lastW, 100, rate), ease(lastH, 16, rate), -0.5f, 0, 200, 32, 200, 32);
+        }
+
+        // 渲染条
+        this.minecraft.getTextureManager().bindTexture(BAR);
+        if (is0to1) rate = 1;
+        if (status == 2) {
+            if (chaos > 0) {
+                blit(matrixStack, ease(width - 33 + 30 - 30 * chaos / 100, width - 66 + 60 - 60 * chaos / 100, rate),
+                        ease(6, height + 23, rate),
+                        ease(30 * chaos / 100, 60 * chaos / 100, rate),
+                        ease(4, 6, rate),
+                        ease(60 - (int) (60 * chaos / 100f), 0.5f + 60 - (int) (60 * chaos / 100f), rate),
+                        1,
+                        60 * chaos / 100, 6, 60, 15);
+            } else if (chaos < 0) {
+                blit(matrixStack, width + ease(3, 6, rate),
+                        ease(6, height + 23, rate),
+                        ease(30 + 30 * chaos / 100, 60 + 60 * chaos / 100, rate),
+                        ease(4, 6, rate),
+                        0.5f,
+                        8,
+                        60 + 60 * chaos / 100, 6, 60, 15);
+            }
+        } else {
+            if (chaos > 0) {
+                blit(matrixStack, ease(width - 66 + 60 - 60 * chaos / 100, width - 33 + 30 - 30 * chaos / 100, rate),
+                        ease(height + 23, 6, rate),
+                        ease(60 * chaos / 100, 30 * chaos / 100, rate),
+                        ease(6, 4, rate),
+                        ease(0.5f + 60 - (int) (60 * chaos / 100f), 60 - (int) (60 * chaos / 100f), rate),
+                        0.5f,
+                        60 * chaos / 100, 7, 60, 15);
+            } else if (chaos < 0) {
+                blit(matrixStack, width + ease(6, 3, rate),
+                        ease(height + 23, 6, rate),
+                        ease(60 + 60 * chaos / 100, 30 + 30 * chaos / 100, rate),
+                        ease(6, 4, rate),
+                        0.5f,
+                        ease(7.5f, 8, rate),
+                        60 + 60 * chaos / 100, 7, 60, 15);
+            }
+        }
+
+
+        // 还原透明度
+        RenderSystem.depthMask(true);
+        RenderSystem.enableDepthTest();
+        RenderSystem.enableAlphaTest();
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+
+
+    }
+
+    private static int ease(int start, int end, float rate) {
+        return (int) (start + (end - start) * rate);
+    }
+
+    private static float ease(float start, float end, float rate) {
+        return start + (end - start) * rate;
     }
 }
