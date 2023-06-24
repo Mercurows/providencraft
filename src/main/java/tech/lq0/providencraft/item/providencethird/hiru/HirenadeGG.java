c22a8c086b083100c22d11db4163e02d62418c8d
17146 <1714673995@qq.com>
2023-06-24 19:35:57 +0800

    添加了守瑠蛋的功能和材质

--- src/main/java/tech/lq0/providencraft/item/providencethird/hiru/HirenadeGG.java
+++ src/main/java/tech/lq0/providencraft/item/providencethird/hiru/HirenadeGG.java
@@ -1,14 +1,20 @@
 package tech.lq0.providencraft.item.providencethird.hiru;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import tech.lq0.providencraft.capability.ChaosHelper;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.tools.Livers;
 import tech.lq0.providencraft.tools.TooltipTool;
@@ -18,8 +24,11 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
 public class HirenadeGG extends Item {
+    private static final Food food = (new Food.Builder()).saturation(1).hunger(5).setAlwaysEdible().
+            effect(() -> new EffectInstance(Effects.WITHER, 200, 0), 1.0f).build();
+
     public HirenadeGG(){
-        super(new Properties().group(ModGroup.itemgroup));
+        super(new Properties().group(ModGroup.itemgroup).food(food));
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -28,6 +37,21 @@ public class HirenadeGG extends Item {
         TooltipTool.addDevelopingText(tooltip);
 
         tooltip.add((new TranslationTextComponent("des.providencraft.hirenade_gg")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addChaosInfo(tooltip, 50);
         TooltipTool.addLiverInfo(tooltip, Livers.HIRU);
+    }
+
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        if(!worldIn.isRemote && entityLiving instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) entityLiving;
+            ChaosHelper.setChaos(player, 50);
+        }
+        return super.onItemUseFinish(stack, worldIn, entityLiving);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack) {
+        return 100;
     }
 }
