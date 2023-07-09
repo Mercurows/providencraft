0847fbca9b4790b4a356e7b7ed62f29ae3bf9c6a
17146 <1714673995@qq.com>
2023-07-09 22:35:21 +0800

    注册了轻岚伞

--- src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
+++ src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
@@ -224,6 +224,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> EYE_MASK = ITEMS.register("eye_mask", EyeMask::new);
     public static final RegistryObject<Item> BREEZE_CROWN = ITEMS.register("breeze_crown", BreezeCrown::new);
     public static final RegistryObject<Item> CHIRAM_CORE = ITEMS.register("chiram_core", ChiramCore::new);
+    public static final RegistryObject<Item> QINGLAN_UMBRELLA = ITEMS.register("qinglan_umbrella", QinglanUmbrella::new);
 
     //haine
     public static final RegistryObject<Item> HAI_PLATE = ITEMS.register("hai_plate", HaiPlate::new);
