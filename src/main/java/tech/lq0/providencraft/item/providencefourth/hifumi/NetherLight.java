3157a69d7dc61a04cace1da84afa76af44009a88
17146 <1714673995@qq.com>
2023-07-27 23:54:13 +0800

    添加了冥界之光的功能

--- src/main/java/tech/lq0/providencraft/item/providencefourth/hifumi/NetherLight.java
+++ src/main/java/tech/lq0/providencraft/item/providencefourth/hifumi/NetherLight.java
@@ -1,9 +1,12 @@
 package tech.lq0.providencraft.item.providencefourth.hifumi;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.SwordItem;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -29,5 +32,23 @@ public class NetherLight extends SwordItem {
         tooltip.add((new TranslationTextComponent("des.providencraft.nether_light_1")).mergeStyle(TextFormatting.GRAY));
         tooltip.add((new TranslationTextComponent("des.providencraft.nether_light_2")).mergeStyle(TextFormatting.GRAY));
         TooltipTool.addLiverInfo(tooltip, Livers.HIFUMI);
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addPotionEffect(new EffectInstance(Effects.STRENGTH, 80, 1));
+        target.addPotionEffect(new EffectInstance(Effects.HASTE, 80, 0));
+        target.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 80, 0));
+
+        attacker.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 40, 0));
+        attacker.addPotionEffect(new EffectInstance(Effects.GLOWING, 40, 0));
+
+        double random = Math.random();
+        if(random < 0.123){
+            target.addPotionEffect(new EffectInstance(Effects.WITHER, 80, 0));
+            attacker.addPotionEffect(new EffectInstance(Effects.REGENERATION, 100, 0));
+        }
+
+        return super.hitEntity(stack, target, attacker);
     }
 }
