5fd17f69b5330f83905f9866b013bd45c43725e7
17146 <1714673995@qq.com>
2023-07-29 01:28:02 +0800

    添加了小春卷的材质

--- src/main/java/tech/lq0/providencraft/integration/vrc/item/chiharu/LittleSpringRoll.java
+++ src/main/java/tech/lq0/providencraft/integration/vrc/item/chiharu/LittleSpringRoll.java
@@ -4,6 +4,8 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.*;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -15,7 +17,9 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class LittleSpringRoll extends Item {
-    private static final Food food = new Food.Builder().setAlwaysEdible().hunger(6).saturation(0.35f).build();
+    private static final Food food = new Food.Builder().setAlwaysEdible().hunger(6).saturation(0.35f)
+            .effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 200, 0), 1.0f)
+            .effect(() -> new EffectInstance(Effects.HUNGER, 300, 0), 0.5f).build();
 
     public LittleSpringRoll(){
         super(new Properties().group(ModGroup.integrationgroup).food(food));
