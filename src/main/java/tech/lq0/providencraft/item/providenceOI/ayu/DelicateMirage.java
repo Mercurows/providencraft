8584bd9f125f3d230ed47ef3e3b47704748f5a23
17146 <1714673995@qq.com>
2023-07-31 02:45:11 +0800

    添加了温柔蜃景的功能

--- src/main/java/tech/lq0/providencraft/item/providenceOI/ayu/DelicateMirage.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/ayu/DelicateMirage.java
@@ -20,6 +20,9 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import tech.lq0.providencraft.capability.ChaosHelper;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.tools.ItemNBTTool;
@@ -30,6 +33,7 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
+@SuppressWarnings({"FieldCanBeLocal", "unused"})
 public class DelicateMirage extends Item {
     public static final String TAG_ATTACK = "attack";
     private final Multimap<Attribute, AttributeModifier> attributeModifiers;
@@ -54,7 +58,7 @@ public class DelicateMirage extends Item {
 
     private Multimap<Attribute, AttributeModifier> getModifiers(ItemStack stack){
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "delicate mirage modifier",
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier",
                         ItemNBTTool.getInt(stack, TAG_ATTACK, 0), AttributeModifier.Operation.ADDITION));
         builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier",
                 -1, AttributeModifier.Operation.ADDITION));
@@ -63,7 +67,7 @@ public class DelicateMirage extends Item {
 
     @Override
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot, ItemStack stack) {
-        return equipmentSlot == EquipmentSlotType.MAINHAND ? getModifiers(stack) : super.getAttributeModifiers(equipmentSlot);
+        return equipmentSlot == EquipmentSlotType.MAINHAND ? getModifiers(stack) : super.getAttributeModifiers(equipmentSlot, stack);
     }
 
     @Override
@@ -72,7 +76,7 @@ public class DelicateMirage extends Item {
             PlayerEntity player = (PlayerEntity) entityIn;
             int chaos = ChaosHelper.getChaos(player);
 
-            ItemNBTTool.setInt(stack, TAG_ATTACK, Math.max(0,chaos / 20));
+            ItemNBTTool.setInt(stack, TAG_ATTACK, Math.max(0,chaos / 10));
         }
     }
 
@@ -82,48 +86,52 @@ public class DelicateMirage extends Item {
             PlayerEntity player = (PlayerEntity) attacker;
 
             int chaos = ChaosHelper.getChaos(player);
-            System.out.println("chaos= " + chaos);
-
-            boolean flag = false;
-            double prob = chaos * 0.01;
+            double prob = Math.abs(chaos) * 0.01;
             double rand = Math.random();
+            int level = Math.min(9, Math.abs(chaos) / 10);
+            float damage = 50 - level * 5.0f;
 
             //混沌时，攻击生物有概率对自己造成伤害
-            if(chaos > 0){
+            if(chaos >= 0){
                 if(rand < prob){
-                    flag = true;
+                    player.attackEntityFrom(DamageSource.MAGIC, player.getHealth() * 0.1f);
 
-                    player.attackEntityFrom(DamageSource.causePlayerDamage(player), player.getHealth() * 0.1f);
+                    ChaosHelper.setChaos(player, Math.max(0, ChaosHelper.getChaos(player) - 5));
                 }
             }
+            //清楚时，攻击生物有概率造成二次伤害
+            if(chaos <= 0){
+                if(rand < prob){
+                    ChaosHelper.setChaos(player, Math.min(0, ChaosHelper.getChaos(player) + 5));
 
-//            new Object() {
-//                private int ticks = 0;
-//                private float waitTicks;
-//
-//                public void start(int waitTicks) {
-//                    this.waitTicks = waitTicks;
-//                    MinecraftForge.EVENT_BUS.register(this);
-//                }
-//
-//                @SubscribeEvent
-//                public void tick(TickEvent.ServerTickEvent event) {
-//                    if (event.phase == TickEvent.Phase.END) {
-//                        this.ticks++;
-//                        if (this.ticks >= this.waitTicks) {
-//                            run();
-//                        }
-//                    }
-//                }
-//
-//                private void run() {
-//                    target.attackEntityFrom(DamageSource.causePlayerDamage(player), 3.0f);
-//
-//                    MinecraftForge.EVENT_BUS.unregister(this);
-//                }
-//            }.start((int) 10);
+                    new Object() {
+                        private int ticks = 0;
+                        private float waitTicks;
+
+                        public void start(int waitTicks) {
+                            this.waitTicks = waitTicks;
+                            MinecraftForge.EVENT_BUS.register(this);
+                        }
+
+                        @SubscribeEvent
+                        public void tick(TickEvent.ServerTickEvent event) {
+                            if (event.phase == TickEvent.Phase.END) {
+                                this.ticks++;
+                                if (this.ticks >= this.waitTicks) {
+                                    run();
+                                }
+                            }
+                        }
+
+                        private void run() {
+                            target.attackEntityFrom(DamageSource.causePlayerDamage(player), damage);
+
+                            MinecraftForge.EVENT_BUS.unregister(this);
+                        }
+                    }.start((int) 20);
+                }
+            }
         }
-
 
         return super.hitEntity(stack, target, attacker);
     }
