9a03f180c34b3d4d4be6d79842d750581f6f2e7a
17146 <1714673995@qq.com>
2023-07-31 16:24:52 +0800

    添加了温柔蜃景的部分方法

--- src/main/java/tech/lq0/providencraft/item/providenceOI/ayu/DelicateMirage.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/ayu/DelicateMirage.java
@@ -2,6 +2,9 @@ package tech.lq0.providencraft.item.providenceOI.ayu;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -13,7 +16,9 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -39,7 +44,7 @@ public class DelicateMirage extends Item {
     private final Multimap<Attribute, AttributeModifier> attributeModifiers;
 
     public DelicateMirage() {
-        super(new Properties().rarity(Rarity.UNCOMMON).group(ModGroup.itemgroup).maxDamage(480));
+        super(new Properties().rarity(Rarity.RARE).group(ModGroup.itemgroup).maxDamage(480));
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
         builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", 1, AttributeModifier.Operation.ADDITION));
         builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -1, AttributeModifier.Operation.ADDITION));
@@ -133,6 +138,36 @@ public class DelicateMirage extends Item {
             }
         }
 
-        return super.hitEntity(stack, target, attacker);
+        stack.damageItem(1, attacker, (entity) -> entity.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+        return true;
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        if (state.getBlockHardness(worldIn, pos) != 0.0F) {
+            stack.damageItem(2, entityLiving, (entity) -> entity.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean canHarvestBlock(BlockState blockIn) {
+        return blockIn.isIn(Blocks.COBWEB);
+    }
+
+    @Override
+    public boolean canPlayerBreakBlockWhileHolding(BlockState state, World worldIn, BlockPos pos, PlayerEntity player) {
+        return !player.isCreative();
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, BlockState state) {
+        if (state.isIn(Blocks.COBWEB)) {
+            return 15.0F;
+        } else {
+            Material material = state.getMaterial();
+            return material != Material.PLANTS && material != Material.TALL_PLANTS && material != Material.CORAL && !state.isIn(BlockTags.LEAVES) && material != Material.GOURD ? 1.0F : 1.5F;
+        }
     }
 }
